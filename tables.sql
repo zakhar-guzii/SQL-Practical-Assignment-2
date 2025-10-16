1abd04f2345bc012cd01fb6a9642a59b9209ebe2
Zakhar Guzii <zakharguzii@gmail.com>
2025-10-17 00:48:41 +0300

    just some debagging in tables.sql

--- tables.sql
+++ tables.sql
@@ -3,32 +3,34 @@ use blog_platform;
 
 create table users (
     user_id int primary key auto_increment,
-    username varchar(50) unique not null,
+    username varchar(50) unique,
     display_name varchar(50),
     creation_date datetime
 );
 
 create table posts (
     post_id int primary key auto_increment,
-    title varchar(255) not null,
+    title varchar(255),
     category enum(
         'technology', 'business-startups', 'education', 'science', 'culture', 'travel',
         'health-sport', 'food-recipes', 'lifestyle', 'art', 'design', 'marketing',
         'programming', 'movies-series', 'music', 'economics', 'politics', 'ecology',
         'psychology', 'history', 'social-media', 'games', 'books', 'analytics', 'interviews'
-    ) not null,
+    ),
     author_id int,
     published_at datetime,
-    comments_count int default 0,
+    comments_count int,
     foreign key (author_id) references users(user_id)
 );
 
-create table comments (
-    comment_id int primary key auto_increment,
-    post_id int not null,
-    author_id int,
-    created_at datetime not null,
-    is_approved bool default true,
-    foreign key (post_id) references posts(post_id),
-    foreign key (author_id) references users(user_id)
+create table comments
+(
+    comment_id    int primary key auto_increment,
+    post_id       int,
+    author_id     int,
+    created_at    datetime,
+    is_approved   bool,
+    comments_text text,
+    foreign key (post_id) references posts (post_id),
+    foreign key (author_id) references users (user_id)
 );
